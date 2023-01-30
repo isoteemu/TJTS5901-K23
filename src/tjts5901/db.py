806dbf426c45c414a049dc939d04ed27df8e2bd4
Teemu Autto <teemu@rautakuu.org>
2023-01-30 14:20:07 +0000

    Added comments.

--- src/tjts5901/db.py
+++ src/tjts5901/db.py
@@ -7,8 +7,14 @@ db = MongoEngine()
 def init_db(app):
     """
     Initialize the database connection.
+
+    Fetches the database connection string from the environment variable `MONGO_URL`
+    and, if present, sets the `MONGODB_SETTINGS` configuration variable to use it.
     """
 
+
+    # To keep secrets private, we use environment variables to store the database connection string.
+    # `MONGO_URL` is expected to be a valid MongoDB connection string, see: blah blah blah
     mongodb_url = environ.get("MONGO_URL")
 
     if mongodb_url is not None:
