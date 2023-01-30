51afee4df2f2f2070093d6d124912eaff70f52cf
Teemu Autto <teemu@rautakuu.org>
2023-01-30 13:57:12 +0000

    User model and item model.

--- src/tjts5901/models.py
+++ src/tjts5901/models.py
@@ -1,13 +1,26 @@
+from datetime import datetime
 from .db import db
 
 from mongoengine import (
     StringField,
+    IntField,
+    ReferenceField,
+    DateTimeField,
+    EmailField,
 )
 
 
-
 class User(db.Document):
-    ...
+    """
+    Model representing a user of the auction site.
+    """
+
+    email = EmailField(required=True, unique=True)
+    "The user's email address."
+
+    password = StringField(required=True)
+
+    created_at = DateTimeField(required=True, default=datetime.utcnow)
 
 
 class Item(db.Document):
@@ -15,6 +28,12 @@ class Item(db.Document):
     A model for items that are listed on the auction site.
     """
 
-    title = StringField(max_length=200, required=True)
+    title = StringField(max_length=100, required=True)
+    description = StringField(max_length=2000, required=True)
 
-    
+    starting_bid = IntField(required=True, min_value=0)
+
+    seller = ReferenceField(User, required=True)
+
+    created_at = DateTimeField(required=True, default=datetime.utcnow)
+    closed_at = DateTimeField()
