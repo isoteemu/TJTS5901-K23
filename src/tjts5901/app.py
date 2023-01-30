c1b4dcfded16e35b8fe9f76990f5c12fbcb9fdfd
Teemu Autto <teemu@rautakuu.org>
2023-01-30 13:59:28 +0000

    Factory function adapted from flask tutorial.

--- src/tjts5901/app.py
+++ src/tjts5901/app.py
@@ -8,7 +8,8 @@ Flask tutorial: https://flask.palletsprojects.com/en/2.2.x/tutorial/
 """
 
 from os import environ
-from typing import Dict, Optional
+import os
+from typing import Dict, Literal, Optional
 
 from dotenv import load_dotenv
 from flask import (
@@ -19,6 +20,7 @@ from flask import (
 )
 
 from .utils import get_version
+from .db import init_db
 
 
 def create_app(config: Optional[Dict] = None) -> Flask:
@@ -29,11 +31,26 @@ def create_app(config: Optional[Dict] = None) -> Flask:
     """
     flask_app = Flask(__name__, instance_relative_config=True)
 
-    if config:
+    flask_app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    # load the instance config, if it exists, when not testing
+    if config is None:
+        flask_app.config.from_pyfile('config.py', silent=True)
+    else:
         flask_app.config.from_mapping(config)
 
-    # Set flask config variable for "rich" loggin from environment variable.
-    flask_app.config.from_envvar("RICH_LOGGING", silent=True)
+    # ensure the instance folder exists
+    try:
+        os.makedirs(flask_app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @flask_app.route('/hello')
+    def hello():
+        return 'Hello, World!'
 
     # Register blueprints
     from . import views  # pylint: disable=import-outside-toplevel
@@ -47,17 +64,10 @@ def create_app(config: Optional[Dict] = None) -> Flask:
 load_dotenv()
 
 # Create the Flask application.
-app = create_app()
-
-# Initialize "rich" output if enabled. It produces more human readable logs.
-# You need to install `flask-rich` to use this.
-if app.config.get("RICH_LOGGING"):
-    from flask_rich import RichApplication
-    RichApplication(app)
-    app.logger.info("Using [blue]rich[/blue] interface for logging")
+flask_app = create_app()
 
 
-@app.route("/server-info")
+@flask_app.route("/server-info")
 def server_info() -> Response:
     """
     A simple endpoint for checking the status of the server.
