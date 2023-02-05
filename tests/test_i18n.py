02fc8e57941cf289736e14d2df7a8b8c527d8750
Teemu Autto <teemu@rautakuu.org>
2023-02-05 00:06:33 +0000

    Test to verify sufficient translation status.

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -12,7 +12,9 @@ from flask_babel import (
     Locale,
     force_locale,
     gettext,
+    get_translations,
 )
+from babel.messages.extract import extract_from_dir
 
 from tjts5901.i18n import SupportedLocales
 
@@ -97,3 +99,58 @@ def test_app_language_detection(client, babel):
         response = client.get('/hello', headers={'Accept-Language': locale.language})
         resp_as_string = response.data.decode('utf-8')
         assert gettext("Hello, World!") != resp_as_string, f"Message is not translated for language {locale.language}"
+
+
+
+@pytest.fixture(scope="session")
+def app_strings():
+    """
+    Fixture for extracting strings from the application source code.
+    """
+
+    # TODO: Read method_map from config file
+    method_map = [
+        ('**.py', 'python'),
+        ('**/templates/**.html', 'jinja2'),
+    ]
+
+    # Collect all of the messages from the source code
+    messages = set()
+    for msg in extract_from_dir('src', method_map):
+        messages.add(msg[2])
+
+    return messages
+
+@pytest.mark.parametrize("locale", SupportedLocales)
+def test_app_translation_status(locale, app, babel, app_strings, fail_treshold=0.15):
+    """
+    Check that the majority of strings in application are translated.
+
+    This test will fail if the percentage of untranslated strings is greater
+    than the :param:`fail_treshold`.
+
+    :param fail_treshold:  Acceptable percentile for untraslated strings.
+    """
+    unique_messages = len(app_strings)
+
+    with app.app_context():
+        locale = Locale.parse(locale.value)
+
+        if locale.language == "en":
+            # By default everything should be in english
+            return
+
+        with force_locale(locale):
+            untranslated_messages = 0
+            # Get the catalog for the current locale, and check if the extracted message
+            # is in the catalog
+            catalog = get_translations()._catalog  # pylint: disable=protected-access
+            for msg in app_strings:
+                # If the message is not in the catalog, then it is untranslated
+                if catalog.get(msg, "") == "":
+                    untranslated_messages += 1
+
+            # Calculate the percentage of untranslated messages
+            untranslated_percent = untranslated_messages / unique_messages
+
+            assert untranslated_percent < fail_treshold, f"Too many untranslated strings for language {locale.language} ({untranslated_percent:.2%})"
