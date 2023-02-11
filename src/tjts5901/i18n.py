212e62ffaea6e96dfb1489224762535dbf8d7831
Teemu Autto <teemu@rautakuu.org>
2023-02-11 17:45:13 +0000

    Absolute path for translations.

--- src/tjts5901/i18n.py
+++ src/tjts5901/i18n.py
@@ -2,6 +2,7 @@
 Internationalisation and localisation support for the application.
 """
 from enum import Enum
+import os
 from typing import List
 from flask_babel import Babel, get_locale as get_babel_locale
 from babel import Locale
@@ -51,9 +52,12 @@ def init_babel(flask_app: Flask):
     system_language = Locale.default().language
     flask_app.config.setdefault("BABEL_DEFAULT_LOCALE", system_language)
 
-    # TODO: Set the default timezone from underlying OS.
+    translation_dir = os.path.join(os.path.dirname(__file__), "translations")
+    flask_app.config.setdefault("BABEL_TRANSLATION_DIRECTORIES", translation_dir)
 
+    # TODO: Set the default timezone from underlying OS.
     babel = Babel(flask_app, locale_selector=get_locale)
+
 
     # Register `locales` as jinja variable to be used in templates. Uses the
     # `Locale` class from the Babel library, so that the locale names can be
